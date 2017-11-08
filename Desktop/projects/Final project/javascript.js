3792b87a7acae4191981e11119d76a2a7b426d25
Daria Ros <dariaros90@gmail.com>
2017-11-08 22:13:42 +0100

    updated the files

--- Desktop/projects/Final project/javascript.js
+++ Desktop/projects/Final project/javascript.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
         //This is onMouseOut event
         $(this).find('.recipe-description').fadeOut(350);
     });
-});
-$(document).ready(function() {
     $(".expanded").hide();
+
+    $(".expanded a, .collapsed a").click(function(eve) {
+        var $container = $(this).parents("div");
+        eve.preventDefault();
+        $container.children(".expanded, .collapsed").toggle();
+    });
+    $( window ).resize(function() {
+        var new_width = $(".recipe-description").prev().width()
+        $(".recipe-description").width(new_width)
+    });
 });
